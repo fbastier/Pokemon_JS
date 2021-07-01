9341afcd402916168819e23a3d92df9988a4fc72
fbastier <francois.bastier@gmail.com>
2021-07-01 17:12:12 +0200

    Il restait de la poussière !

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 function Premier(pkm1, pkm2) {
     // tirage au sort 
     let val = Math.random();
@@ -15,6 +13,8 @@ function Premier(pkm1, pkm2) {
 function affichagePoints(pkm) {
     console.log(pkm.getName() + " a " + pkm.getLifePoints() + " points de vie");
 }
+
+
 
 // Instanciation des objets Pikachu et Evoli
 let Pikachu = new Pokemon("Pikachu", 025, 40, 6,"Electric", 82, "statik", 10, "paratonnerre", 25);
@@ -38,12 +38,10 @@ console.log("____________________________________________");
 while (premier.getLifePoints() > 0 && second.getLifePoints() > 0 ) {
     let pVal = premier.attaque(second);
     let firstString = premier.getName() + " a attaqué. Il a fait " + pVal + " de dégâts. ";
-    //console.log("####" + premier.getLifePoints());
     if (second.getLifePoints() < 0) {
         console.log( firstString); 
     } else {
         let sVal = second.attaque(premier);
-        //console.log("####" + second.getLifePoints());
         console.log( firstString + second.getName() + " a attaqué. Il a fait " + sVal + " de dégâts. ");
     }
     
@@ -56,7 +54,3 @@ if (premier.getLifePoints() > 0) {
     gagnant = second;
 }
 console.log(gagnant.getName() + " a gagné le combat. Il lui restait " + gagnant.getLifePoints() + " points de vie.");
-
-
-//console.log(premier.getName() + " a gagné le combat. Il lui restait " + premier.getLifePoints() + " points de vie.");
-//console.log(second.getName() + " a gagné le combat. Il lui restait " + second.getLifePoints() + " points de vie.");
